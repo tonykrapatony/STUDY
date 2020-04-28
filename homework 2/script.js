feb67764272aeff96f8de0f3b5897ae5be33dc38
Ihor <tonykrapatony@gmail.com>
2020-04-28 14:33:43 +0300

    some updates to homework2's styles

--- homework 2/script.js
+++ homework 2/script.js
@@ -2,6 +2,6 @@ function reloadPage(){
     if (confirm("Are you sure you want to reload the page? All completed data will be deleted.")) {
         location.reload();
     } else {
-        alert("You pressed the cancel button.")ж
+        alert("You pressed the cancel button.");
     }
 }
