7d3dbe6c84616eb6363c2ab8e85ae7470133a3d7
Ihor <tonykrapatony@gmail.com>
2020-05-05 18:01:02 +0300

    homework 4 style update

--- homework 4/js/script.js
+++ homework 4/js/script.js
@@ -1,10 +1,16 @@
 $(function(){
-    var width = $(window).width();
-    alert(width);
+    $('.logo-container').on('click', function(){
+        location.reload();
+    })
     $('.burger-button').on('click', function(){
         $('nav').toggleClass('hiden');
+        $('.burger-button').toggleClass('active-burger');
+        
     })
     $('.nav-list-item').on('click', function(){
-        $(this).toggleClass('active');
+        if ( $(this).hasClass('active') != true ){
+            $('.nav-list-item').removeClass('active');
+            $(this).addClass('active');
+        }
     })
 })
